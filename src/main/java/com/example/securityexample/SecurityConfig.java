98a5bda516d69a39c87c84739d3e3175bbed609a
Imana Ohran <imana.ohran.20@size.ba>
2025-02-01 13:44:45 +0100

    Storing password in encrypted format

--- src/main/java/com/example/securityexample/SecurityConfig.java
+++ src/main/java/com/example/securityexample/SecurityConfig.java
@@ -17,6 +17,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
@@ -43,12 +45,12 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user1 = User.withUsername("user1")
-                .password("{noop}password1")
+                .password(passwordEncoder().encode("password1"))
                 .roles("USER")
                 .build();
 
         UserDetails admin = User.withUsername("admin")
-                .password("{noop}password1")
+                .password(passwordEncoder().encode("password2"))
                 .roles("ADMIN")
                 .build();
         JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
@@ -56,5 +58,10 @@ public class SecurityConfig {
         userDetailsManager.createUser(admin);
         return userDetailsManager;
         /* return new InMemoryUserDetailsManager(user1, admin); */
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
